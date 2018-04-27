e8b70475640ccde4d2b645b783299ca037353dc2
Edgar Moreno <ich_bin_edgar@hotmail.com>
2018-04-26 20:50:45 -0500

    CORS issue fix (I hope so)

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const app = express()
 const graphqlHTTP = require("express-graphql")
 const schema = require("./graphql")
 require('dotenv').config()
+const cors = require('cors');
 
 const mongoose = require("mongoose")
 mongoose.connect(process.env.DB_URL)
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use("/graphql", graphqlHTTP({
     schema,
