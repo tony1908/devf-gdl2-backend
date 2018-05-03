ce8d03e901ac25fa0bc42c5d151c35b4bcdcdf85
HomeroValdovinos <31164951+HomeroValdovinos@users.noreply.github.com>
2018-05-02 21:08:10 -0500

    Updating graphql/mutations/index.js
    I added the lines for Reset Passwords

--- graphql/mutations/index.js
+++ graphql/mutations/index.js
@@ -1,15 +1,9 @@
 const User = require("./Users/User")
-<<<<<<< HEAD
 const Login = require("./Login/Login")
-
-module.exports = {
-    User,
-    Login
-=======
 const ResetPassword = require("./Password/ResetPassword")
 
 module.exports = {
     User,
+    Login,
     ResetPassword
->>>>>>> f38b308a33135bf0155415fb11f814c52f7a7d99
 }
